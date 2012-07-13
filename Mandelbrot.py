8acdd69c6fd083a424d1bc383f7711eee72a174f
matt <mattfilmer@gmail.com>
2012-07-12 21:04:21 -0400

    Basically working now still need to implement colorization of the gray images

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,16 +1,37 @@
-
+from __future__ import division
+import Image
 
 class mandelbrot(object):
     def __init__(self):
         pass
     
-    def genImage(self,window,resolution,fileName):
-        pass
+    def genImage(self,(l,t,w,h),res,fileName,mode='Binary'):
+        if not mode in ['Binary','Gray','Color']:
+            error('invalid mode')
+        setImage = Image.new('RGB',(w*res,h*res))
+        setAccess = setImage.load()
+        #example usage:
+        #setAccess[0,0] = (0,0,0)
+        #complex numbers represented as a + bj
+        for a in self.__frange(l,l+w,1/res):        #real part
+            for b in self.__frange(t,t-h,-1/res):    #imag part
+                val = self.getPointCount(complex(a,b))
+                loc = self.__locToPix((l,t,w,h),res,(a,b))
+                if mode == 'Binary':
+                    if val == 25:
+                        setAccess[loc] = (255,)*3
+                    else:
+                        loc = tuple(map(int,loc))
+                        #print loc
+                        setAccess[loc] = (0,)*3
+                elif mode == 'Gray' or mode == 'Color':
+                    setAccess[loc] = ((26-val)*10,)*3
+                    if mode == 'Color':
+                        #colorize the gray image
+                        pass
+        setImage.save(fileName,'PNG')
     
-    def genColorImage(self,window,resolution,fileName):
-        pass
-    
-    def checkPoint(self,c,iterations=25,radius=2):
+    def checkPoint(self,c,iterations=26,radius=2):
         count = self.getPointCount(c,iterations,radius)
         if count == iterations:
             return True
@@ -27,7 +48,12 @@ class mandelbrot(object):
             return iterations
         return i
     
-    def __frange(self, start, stop=None, step=1):
+    def __locToPix(self,(l,t,w,h),res,(x,y)):
+        return ((x-l)*res,-(y-t)*res)
+    
+    def __frange(self,start,stop=None,step=1):
+        if step == 0:
+            error('step cannot be 0')
         if stop == None:
             stop = start
             start = 0
@@ -35,5 +61,9 @@ class mandelbrot(object):
         while True:
             yield i
             i += step
-            if i >= stop:
-                break
+            if step > 0:
+                if i >= stop:
+                    break
+            else:
+                if i <= stop:
+                    break
